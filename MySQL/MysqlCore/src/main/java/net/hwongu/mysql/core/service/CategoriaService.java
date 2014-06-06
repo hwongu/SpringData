ae0cebcd76222496c1a53d591d8f52fa52701e35
HENRY JOE WONG URQUIZA <hwongu@gmail.com>
2014-06-06 15:43:06 -0500

    Commit

--- MySQL/MysqlCore/src/main/java/net/hwongu/mysql/core/service/CategoriaService.java
+++ MySQL/MysqlCore/src/main/java/net/hwongu/mysql/core/service/CategoriaService.java
@@ -3,6 +3,11 @@ package net.hwongu.mysql.core.service;
 import net.hwongu.mysql.core.service.base.BaseService;
 import net.hwongu.mysql.repository.CategoriaRepository;
 
+/**
+ * 
+ * @author Henry Joe Wong Urquiza
+ * @mail hwongu@hwongu.net
+ */
 public interface CategoriaService extends BaseService<CategoriaRepository>{
 
 }
